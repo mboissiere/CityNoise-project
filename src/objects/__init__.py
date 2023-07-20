02525aad6ef2a86edb0919a579fc620866e4a143
mboissiere <mboissiere@proton.me>
2023-07-20 10:06:50 +0200

    refactor: deleted unnecessary inits

--- src/objects/__init__.py
+++
@@ -1,3 +0,0 @@
-"""That way, if I'm lost, confused, change my mind on modelling approach, it won't be a hassle!
-Let's not that I'm not completely excluding the possibility of doing it over FEniCS, and include a gas dispersion
-equation to solve. That being said, it seems very complicated for now, so let's start simple!"""
