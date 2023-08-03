6781e18d58150c286eef780ca7869e1063cbeb08
mboissiere <mboissiere@proton.me>
2023-08-03 21:44:33 +0200

    added KDE deprecation in comment form

--- src/main.py
+++ src/main.py
@@ -65,10 +65,12 @@ for timestep in gdf[TIMESTEP_COLUMN].sort_values().unique():
     geofig.adjustAxesFromGeoDataFrame(gdf)
     accumulation_gdf = addAccumulationDataFromGeoDataFrame(accumulation_gdf, timestep_gdf, input_columns)
 
-    if plot_type == "KDE":
-        geofig.createKDEPlotFromGeoDataFrame(accumulation_gdf, 'accumulated_CO2')
-    elif plot_type == "Histogram":
-        geofig.createHistogramPlotFromGeoDataFrame(accumulation_gdf, 'accumulated_CO2', converted_max, unit)
+    # DEPRECATED
+    # if plot_type == "KDE":
+    #     geofig.createKDEPlotFromGeoDataFrame(accumulation_gdf, 'accumulated_CO2')
+    # elif plot_type == "Histogram":
+
+    geofig.createHistogramPlotFromGeoDataFrame(accumulation_gdf, 'accumulated_CO2', converted_max, unit)
     end_time = time()
 
     elapsed_time = end_time - start_time
